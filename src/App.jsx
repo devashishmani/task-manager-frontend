7822c6b5631820c81c81cb620b4c37da0a5aae2b
Devashish Mani <devashishmani593@gmail.com>
2026-05-01 20:08:00 +0530

    fix routing

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <BrowserRouter>
       <Routes>
             <Route path="/" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+        
         <Route path="/register" element={<Register />} />
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/member" element={<MemberDashboard />} />
